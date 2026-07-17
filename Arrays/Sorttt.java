89e8b938f7edc0bc5135b31e8c9e554249ccbd35
shitiz <kshitijbhardwaj002@gmail.com>
2026-07-17 22:52:31 +0530

    merge two array

--- Arrays/Sorttt.java
+++ Arrays/Sorttt.java
@@ -1,14 +1,24 @@
 import java.util.*;
 
-public class Sorttt{
-    public static void main(String[] args){
+public class Sorttt {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [] arr = new int[n];
 
-        for(int i = 0;i<n;i++){
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+
         Arrays.sort(arr);
+
+        System.out.print("Your sorted array: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
     }
 }
