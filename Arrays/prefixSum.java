d5aab93dee2d8254f3efd785ba574f291412867b
Shitiz bhardwaj <bhardwajkshitij27@gmail.com>
2026-07-19 22:44:50 +0530

    Update prefixSum.java

--- Arrays/prefixSum.java
+++ Arrays/prefixSum.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class prefixSum {
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 4, 6, 9};
-        int[] prefixSum = new int[arr.length];
+        int[] array = {1, 2, 4, 6, 9};
+        int[] prefixSum = new int[array.length];
 
-        prefixSum[0] = arr[0];
+        prefixSum[0] = array[0];
 
-        for (int i = 1; i < arr.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        for (int i = 1; i < array.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + array[i];
         }
 
         System.out.println(Arrays.toString(prefixSum));
